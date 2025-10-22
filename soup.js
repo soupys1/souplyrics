b6cbb5b8da6cd55e963696ed5a27ca52dc4cab51
SouptikSinha <ssinha21@hawk.illinoistech.edu>
2025-10-22 01:25:12 -0500

    old version

--- soup.js
+++ soup.js
@@ -1,12 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config();
 
-
-
-const { Client, GatewayIntentBits } = require('discord.js');
-const { getLyrics } = require('genius-lyrics-api');
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -16,20 +10,20 @@ const client = new Client({
   ],
 });
 
-client.on('ready', () => {
+client.on("clientReady", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
   // Ignore bot messages
   if (message.author.bot) return;
 
   // Command: soup!lyrics <song name>
-  if (message.content.startsWith('soup!lyrics')) {
+  if (message.content.startsWith("soup!lyrics")) {
     const query = message.content.slice(11).trim();
-    
+
     if (!query) {
-      message.reply('Please provide a song name!');
+      message.reply("Please provide a song name!");
       return;
     }
 
@@ -40,33 +34,29 @@ client.on('messageCreate', async (message) => {
 
 async function fetchLyrics(query, message) {
   // TODO: Implement lyrics fetching
-  const [artist, title] = query.split('-');
-  if(!artist || !title){
+  const [artist, title] = query.split("-");
+  if (!artist || !title) {
     message.reply('please provide the song in "Artist-Title" format!');
     return;
   }
-  const options = {
-	apiKey: 'GAPI',
-	title: title.trim(),
-	artist: artist.trim(),
-	optimizeQuery: true
-    };
-    
-    lyrics = getLyrics(options).then((lyrics) => console.log(lyrics));
+  //fetching the lyrics
 
-    if(!lyrics){
-        message.reply('Lyrics not found!');
-    }else{
-        message.reply(lyrics);
-    }
-    
+  fetch(
+    `https://api.lyrics.ovh/v1/${encodeURIComponent(artist)}/${encodeURIComponent(title)}`,
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Lyrics not found");
+      } else {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      message.reply(data.lyrics);
+    })
+    .catch((err) => {
+      message.reply("Lyrics not found!");
+    });
 }
-app.get('/', (req, res) => {
-  res.send('Bot is alive!');
-});
 
-app.listen(port, () => {
-  console.log(`Keep-alive server running on port ${port}`);
-});
 client.login(process.env.BOTTOKEN);
-
